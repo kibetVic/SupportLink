a78a17c87d5131e4476702470ad4fed59cc8e682
Proff-Kibet <Proff-Kibet@PROFF-KIBET>
2025-08-18 12:36:16 +0300

    controllers

---
+++ SupportLink/Controllers/FeedbacksController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SupportLink.Data;
+using SupportLink.Models;
+
+namespace SupportLink.Controllers
+{
+    public class FeedbacksController : Controller
+    {
+        private readonly SupportLinkDbContext _context;
+
+        public FeedbacksController(SupportLinkDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Feedbacks
+        public async Task<IActionResult> Index()
+        {
+            var supportLinkDbContext = _context.Feedbacks.Include(f => f.Ticket).Include(f => f.User);
+            return View(await supportLinkDbContext.ToListAsync());
+        }
+
+        // GET: Feedbacks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedbacks
+                .Include(f => f.Ticket)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Create
+        public IActionResult Create()
+        {
+            ViewData["SupportId"] = new SelectList(_context.SupportTickets, "SupportId", "Description");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email");
+            return View();
+        }
+
+        // POST: Feedbacks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SupportId,UserId,Rating,Comments,CreatedAt")] Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feedback);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SupportId"] = new SelectList(_context.SupportTickets, "SupportId", "Description", feedback.SupportId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", feedback.UserId);
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedbacks.FindAsync(id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+            ViewData["SupportId"] = new SelectList(_context.SupportTickets, "SupportId", "Description", feedback.SupportId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", feedback.UserId);
+            return View(feedback);
+        }
+
+        // POST: Feedbacks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SupportId,UserId,Rating,Comments,CreatedAt")] Feedback feedback)
+        {
+            if (id != feedback.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(feedback);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeedbackExists(feedback.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SupportId"] = new SelectList(_context.SupportTickets, "SupportId", "Description", feedback.SupportId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", feedback.UserId);
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedbacks
+                .Include(f => f.Ticket)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedback);
+        }
+
+        // POST: Feedbacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var feedback = await _context.Feedbacks.FindAsync(id);
+            if (feedback != null)
+            {
+                _context.Feedbacks.Remove(feedback);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeedbackExists(int id)
+        {
+            return _context.Feedbacks.Any(e => e.Id == id);
+        }
+    }
+}
